b0dcead1168023b76419f85f3500a7ed7a802d17
ZJ <92379986+zijian99@users.noreply.github.com>
2021-10-31 21:27:52 +0800

    error checking for all user input

--- code/RRPSS.java
+++ code/RRPSS.java
@@ -28,12 +28,17 @@ public class RRPSS implements Serializable {
 			System.out.println("(4) Sales Report(Admin)");
 			System.out.println("(5) Exit");
 			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-			
-			System.out.print("Enter Your Choice: ");
-			sc = new Scanner(System.in);
-			choice = sc.nextInt();
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-			System.out.println();
+			try {
+				System.out.print("Enter Your Choice: ");
+				sc = new Scanner(System.in);
+				choice = sc.nextInt();
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				System.out.println();
+			}
+			catch(InputMismatchException e)
+			{
+				sc.nextLine();
+			}
 			switch (choice) {
 				case 1:
 					startReservationApp();
@@ -90,19 +95,25 @@ public class RRPSS implements Serializable {
 		Database database = new Database();
 		
 		while (choice != 3) {
-			System.out.println("***Krusty Krab Restaurant Reservation and Point of Sale System Manager***");
-			System.out.println();
-			System.out.println("What do you want?");
-			System.out.println("(1) Create New RRPSS System");
-			System.out.println("(2) Load existing RRPSS System");
-			System.out.println("(3) Exit");
-			
-			System.out.println("*************************************************************************");
-			
-			System.out.print("Enter Your Choice: ");
-			choice = sc.nextInt();
-			System.out.println("*************************************************************************");
-			System.out.println();	
+			try {
+				System.out.println("***Krusty Krab Restaurant Reservation and Point of Sale System Manager***");
+				System.out.println();
+				System.out.println("What do you want?");
+				System.out.println("(1) Create New RRPSS System");
+				System.out.println("(2) Load existing RRPSS System");
+				System.out.println("(3) Exit");
+
+				System.out.println("*************************************************************************");
+
+				System.out.print("Enter Your Choice: ");
+				choice = sc.nextInt();
+				System.out.println("*************************************************************************");
+				System.out.println();
+			}
+			catch(InputMismatchException e)
+			{
+				sc.nextLine();
+			}
 			switch (choice) {
 				case 1:
 					main = new RRPSS();
