0028c8598a4eb19b4d86ff11191528465cde4773
ZJ <92379986+zijian99@users.noreply.github.com>
2021-11-13 18:33:42 +0800

    update javadoc

--- src/AlaCarteItemType.java
+++ src/AlaCarteItemType.java
@@ -6,8 +6,20 @@ import java.io.Serializable;
  * @since 2021-11-5
  */
 public enum AlaCarteItemType implements Serializable {
+	/**
+	 * Main course type of AlaCarte item
+	 */
     MAIN_COURSE,
+    /**
+	 * Appetizer type of AlaCarte item
+	 */
     APPETISER,
+    /**
+	 * Drinks type of AlaCarte item
+	 */
     DRINKS,
+    /**
+	 * Dessert type of AlaCarte item
+	 */
     DESSERT
 }
