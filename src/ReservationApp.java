84c424b3f0c5856ed4421c8f28f85874ec9a8e7f
roylau98 <roylau98@gmail.com>
2021-11-13 22:16:14 +0800

    swap createScheduler() and removeNoShowReservations()

--- src/ReservationApp.java
+++ src/ReservationApp.java
@@ -55,8 +55,8 @@ public class ReservationApp implements Serializable, AppInterface {
      */
     public void openOptions() {
         scanner = new Scanner(System.in);
-        reservationMgr.removeNoShowReservations();
         reservationMgr.createScheduler();
+        reservationMgr.removeNoShowReservations();
         boolean cont = true;
         int choice = 0;
         while (cont) {
